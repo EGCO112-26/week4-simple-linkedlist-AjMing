c64205944d426a179a70d5eeac21243eee2ddabe
AjMing <62439174+AjMing@users.noreply.github.com>
2026-02-02 09:51:42 +0000

    test

--- main.c
+++ main.c
@@ -7,57 +7,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "node.h"
+#include "std_node.h"
 
 int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
-    a.next=&b;
-    head=&a;
-    b.value=head->value+3;
 
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
-/*  Exercise I
-    1. Add 1 more than at the end
-    2. Add value(11)
-    3. Make next become NULL
- */
-    
-/*  Exercise II
-        1. Add 1 more than at the begining!!!!
-        2. Add value (2)
-        
-*/
-    typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-          // What is missing???
-        }
-    */
-    
-   /*  Exercise IV change to while loop!! (you can use NULL to help)
-       
-         while(){
-            printf("%3d", tmp->value);
-           // What is missing???
-        }
-    */
-    
+   NodePtr temp,head;
+
+    printf("Create nodes by malloc\n");
  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
          //use a loop to help
           
      */
+        
+        head=  (NodePtr) malloc(sizeof(Node));
+        temp=head;
+        
+        n=(argc-1)/2;
+        for(i=0;i<n;i++){
+            temp->value =7+i*2;
+            temp ->next=  (NodePtr) malloc( sizeof(Node));
+            temp=temp->next;
+        }
+        temp->value =7+i*2;
+        temp ->next= NULL;
+
+       temp=head;
+         while(temp){  //temp!=NULL
+            printf("%3d", temp->value);
+            temp=temp->next;
+           // What is missing???
+        }
+    printf("\n");
 
     /*  Exercise VI Free all node !!
          //use a loop to help
           
      */
-    
+     temp=head;
+     while(temp){
+          head=head->next;
+          printf("Freeing %d\n",temp->value);
+          free(temp);
+          temp=head;       
+     }
     return 0;
 }
